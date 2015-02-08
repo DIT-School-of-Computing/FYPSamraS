38263b13d4b5e4aeed5375ef25bcf8b785ce1776
Stephen Samra <stephensamra2@gmail.com>
2015-02-08 14:21:02 +0000

    Added documentation.

--- calculations.c
+++ calculations.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// Stephen Samra - C11537047
+// Assignment 1: "Probably the most complex system in the known universe"
+
+// A function that takes two numbers and returns the product
 int multiply(int a, int b)
 {
     return a * b;
@@ -7,10 +11,13 @@ int multiply(int a, int b)
 
 void main()
 {
+    // Two numbers necessary for this program to function
     int a = 5;
     int b = 10;
 
+    // Where the magic happens
     int c = multiply(a, b);
 
+    // Inform the user of said magic
     printf("The value of c is: %d", c);
 }
