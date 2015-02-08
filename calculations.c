79fd27f80bc3e9021710b100dd5f71d3ac93b6a0
Stephen Samra <stephensamra2@gmail.com>
2015-02-08 13:39:03 +0000

    Added a multiplication function.

--- calculations.c
+++ calculations.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
 void main()
 {
     int a = 5;
     int b = 10;
 
-    int c = a * b;
+    int c = multiply(a, b);
 
     printf("The value of c is: %d", c);
 }
